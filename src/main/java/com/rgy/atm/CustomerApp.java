40ad5807551c9390a61e33e4ee15ed386cc7089f
unknown <rgylan74@gmail.com>
2019-08-25 20:49:43 +0800

    Initial commit.

---
+++ src/main/java/com/rgy/atm/CustomerApp.java
@@ -0,0 +1,14 @@
+package com.rgy.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CustomerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApp.class, args);
+    }
+}
