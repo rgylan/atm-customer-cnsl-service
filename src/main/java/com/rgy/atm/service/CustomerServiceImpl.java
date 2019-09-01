29d9f4c2d5aed0b9fcc9dfb44b83b7a02b69c9d6
unknown <rgylan74@gmail.com>
2019-09-01 23:15:04 +0800

    Use implementing class as log class.

--- src/main/java/com/rgy/atm/service/CustomerServiceImpl.java
+++ src/main/java/com/rgy/atm/service/CustomerServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerServiceImpl.class);
 
     private final CustomerRepository customerRepository;
 
